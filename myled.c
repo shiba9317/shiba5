4f9e893c7fd49194aed83aede8a37ca9aa5133d5
shiba9317 <92848929+shiba9317@users.noreply.github.com>
2022-01-17 23:15:33 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -21,71 +21,89 @@ static struct class *cls = NULL;
 
 static volatile u32 *gpio_base = NULL;
 
-static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
-{
-char c;
-if(copy_from_user(&c,buf,sizeof(char)))
-return -EFAULT;
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){
 
-if(c == '0'){
-gpio_base[10] = 1 << 25;
-}
-else if(c == '1'){
-gpio_base[7] = 1 << 25;
-}
-return 1;
+        char c;
+
+        if(copy_from_user(&c,buf,sizeof(char))){
+
+                return -EFAULT;
+        }
+        if(c == '0'){
+
+                gpio_base[10] = 1 << 25;
+
+        }
+        else if(c == '1'){
+
+                gpio_base[7] = 1 << 25;
+
+        }
+
+        return 1;
+
 }
 
 static struct file_operations led_fops = {
-.owner = THIS_MODULE,
-.write = led_write
+
+        .owner = THIS_MODULE,
+        .write = led_write
+
 };
 
-static int __init init_mod(void)
-{
-int retval;
+static int __init init_mod(void){
+ 
+ int retval;
+        gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+        const u32 led = 25;
+        const u32 index = led/10;
+        const u32 shift = (led%10)*3;
+        const u32 mask = ~(0x7 << shift);
+        gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+        retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
 
-gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+        if(retval < 0){
 
-const u32 led = 25;
-const u32 index = led/10;
-const u32 shift = (led%10)*3;
-const u32 mask = ~(0x7 << shift);
-gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+                printk(KERN_ERR "alloc_chrdev_region failed.\n");
+                return retval;
 
- retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
-if(retval < 0){
-printk(KERN_ERR "alloc_chrdev_region failed.\n");
-return retval;
+        }
+
+        printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+        cdev_init(&cdv, &led_fops);
+        cdv.owner = THIS_MODULE;
+        retval = cdev_add(&cdv, dev, 1);
+
+        if(retval < 0){
+
+                printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+                return retval;
+
+        }
+
+        cls = class_create(THIS_MODULE,"myled");
+
+        if(IS_ERR(cls)){
+
+                printk(KERN_ERR "class_create failed.");
+                return PTR_ERR(cls);
+
+        }
+
+        device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+        return 0;
+
 }
-printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
-cdev_init(&cdv, &led_fops);
-cdv.owner = THIS_MODULE;
-retval = cdev_add(&cdv, dev, 1);
-if(retval < 0){
-printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
-return retval;
-}
+static void __exit cleanup_mod(void){
 
-cls = class_create(THIS_MODULE,"myled");
-if(IS_ERR(cls)){
-printk(KERN_ERR "class_create failed.");
-return PTR_ERR(cls);
-}
-device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+        cdev_del(&cdv);
+        device_destroy(cls, dev);
+        class_destroy(cls);
+        unregister_chrdev_region(dev, 1);
+        printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+        iounmap(gpio_base);
 
-return 0;
-}
-
-static void __exit cleanup_mod(void)
-{
-cdev_del(&cdv);
-device_destroy(cls, dev);
-class_destroy(cls);
-unregister_chrdev_region(dev, 1);
-printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
-iounmap(gpio_base);
 }
 
 module_init(init_mod);
